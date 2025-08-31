66d684f5164391fb6a416bf52d6d352ad59ed6e8
Mikael Kalms <mikael@kalms.org>
2025-08-31 18:59:39 +0200

    Allow External Secret Operator access to secrets in other apps' GCP projects

--- apps/external-secrets/tf/gke/infrastructure/workload_identity/workload_identity.tf
+++ apps/external-secrets/tf/gke/infrastructure/workload_identity/workload_identity.tf
@@ -17,3 +17,27 @@ resource "google_service_account_iam_member" "workload_identity_user" {
   role               = "roles/iam.workloadIdentityUser"
   service_account_id = google_service_account.cluster_service_account.id
 }
+
+# Retrieve ancestry of the '${prefix}-external-secrets' project
+# This should be a folder
+# Reference: https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/project_ancestry
+data "google_project_ancestry" "project_ancestry" {
+}
+
+# Grant read access for all apps' Secrets to External Secret Operator
+# Reference: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_folder_iam#google_folder_iam_member
+resource "google_folder_iam_member" "secret_accessor" {
+
+  # Locate the folder that contains all apps within the managed cluster
+  # Reference: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_folder_iam#folder-1
+  folder = "folders/${data.google_project_ancestry.project_ancestry.ancestors[1].id}"
+
+  # Grant capability to read secrets
+  # Reference: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_folder_iam#role-1
+  # Reference: https://cloud.google.com/iam/docs/roles-permissions/secretmanager
+  role   = "roles/secretmanager.secretAccessor"
+
+  # Grant it to External Secret Operator's Service Account
+  # Reference: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_folder_iam#member/members-1
+  member = "serviceAccount:${google_service_account.cluster_service_account.email}"
+}
